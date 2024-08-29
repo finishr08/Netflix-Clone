6c3eac6cfe1428899441f3a304d1f94d636df8f9
Mustafa Ahmed <contactmustafa8@gmail.com>
2024-08-29 13:22:33 +0500

    Netflix-Clone

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,12 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { ENV_VARS } from "../config/envVars";
 
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:5000",
+        target: ENV_VARS.VITE_API_URL,
         changeOrigin: true,
         secure: false,
       },
